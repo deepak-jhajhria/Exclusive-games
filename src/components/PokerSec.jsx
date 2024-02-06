d0714557e3be77969986846ca29ed9433fe349df
Deepak jaat <deepakjhajhria4657@gmail.com>
2024-02-06 09:11:14 +0530

    update

--- src/components/PokerSec.jsx
+++ src/components/PokerSec.jsx
@@ -8,7 +8,7 @@ const PokerSec = () => {
             <div className="container -mt-4 max-w-[1140px] pb-8 sm:pb-12 md:pb-20 lg:pb-[100px] relative z-30">
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-8 sm:gap-10 md:gap-[64px] w-full">
                     <div className="flex flex-col">
-                        <div data-aos="fade-up" data-aos-duration="500"><img className='w-[90%] mx-auto md:w-full order-2 md:order-1' src={img1} alt="poker with friends" /></div>
+                        <div className='order-2 md:order-1' data-aos="fade-up" data-aos-duration="500"><img className='w-[90%] mx-auto md:w-full' src={img1} alt="poker with friends" /></div>
                         <div className='flex flex-col max-sm:mb-7 max-md:mb-10 md:mt-[60px] max-w-[444px] order-0 md:order-2'>
                             <h2 data-aos="fade-up" data-aos-duration="500" className='text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-normal font-Anton lg:leading-[57px] text-white'>Nuestra Visión</h2>
                             <p data-aos="fade-up" data-aos-duration="500" className='mt-2 text-sm font-medium text-white sm:mt-3 md:mt-4 sm:text-base font-Inter opacity-80'>Nuestra visión es ser líderes indiscutibles en la industria de los juegos de azar y llevar nuestra pasión por el entretenimiento más allá de las fronteras. Imagina un mundo donde la emoción y la diversión no tengan límites, y ese es el mundo que queremos crear contigo.</p>
